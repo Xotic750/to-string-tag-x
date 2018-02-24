c311201e7d9ba32a14ea9aaf518bc33974760a10
Graham Fairweather <xotic750@gmail.com>
2018-02-24 13:07:48 +0100

    Update dependencies

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 /**
  * @file Get an object's ES6 @@toStringTag.
  * @see {@link http://www.ecma-international.org/ecma-262/6.0/#sec-object.prototype.tostring|19.1.3.6 Object.prototype.toString ( )}
- * @version 1.4.2
+ * @version 1.4.3
  * @author Xotic750 <Xotic750@gmail.com>
  * @copyright  Xotic750
  * @license {@link <https://opensource.org/licenses/MIT> MIT}
