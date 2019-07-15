a958e73698c5103edf493ef67d0a2cfed1f57926
Graham Fairweather <xotic750@gmail.com>
2019-07-15 12:30:01 +0200

    :bookmark: v2.0.8
    Auto build

--- dist/to-string-tag-x.js
+++ dist/to-string-tag-x.js
@@ -2,13 +2,13 @@
 {
   "author": "Graham Fairweather",
   "copywrite": "Copyright (c) 2015-present",
-  "date": "2019-07-14T21:34:02.364Z",
+  "date": "2019-07-15T10:29:57.618Z",
   "describe": "",
   "description": "Get an object's ES6 @@toStringTag.",
   "file": "to-string-tag-x.js",
   "hash": "65b1228d127b413893de",
   "license": "MIT",
-  "version": "2.0.7"
+  "version": "2.0.8"
 }
 */
 (function webpackUniversalModuleDefinition(root, factory) {
